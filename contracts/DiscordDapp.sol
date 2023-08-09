fec06a51d936ceed1de01a79b36f2a2dedff3e26
Adityaweb3 <ap7596112@gmail.com>
2023-08-09 22:37:29 +0530

    createChannel

--- contracts/DiscordDapp.sol
+++ contracts/DiscordDapp.sol
@@ -5,13 +5,35 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DiscorDapp is ERC721 {
 
+    uint256 public totalChannels ; 
+
     address public owner ; 
+
+    struct Channel {
+        uint256 id ;
+        string name ;
+        uint256 cost ;
+    }
+
+    mapping(uint256 =>Channel) public channels ;
 
 
    constructor(string memory _name , string memory _symbol) ERC721 (_name , _symbol){
 
     owner = msg.sender ;
-    
+
+
+
+}
+
+function createChannel(string memory _name, uint256 _cost) public {
+
+    totalChannels++ ;
+
+    channels[totalChannels]= Channel(totalChannels, _name ,_cost);
+
+
+
 
 
 }
